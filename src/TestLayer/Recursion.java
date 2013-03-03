ca2a4c955f7937773750aa0595493b38cd69b95b
Jakub Kozlowski <kooba007@gmail.com>
2013-03-03 22:57:04 +0100

    recursion

--- src/TestLayer/Recursion.java
+++ src/TestLayer/Recursion.java
@@ -3,7 +3,14 @@ package TestLayer;
 public class Recursion {
 	
 	
-	public void 
+	public void nwd(int a, int b){
+		if(b != 0){
+			nwd(a,b);
+		}
+		else{
+			
+		}
+	}
 	
 	public static void main(String [] a){
 		
